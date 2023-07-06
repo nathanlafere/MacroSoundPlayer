a328d43e8d50e0c35dfcb3fe9d44d61721e90560
Nathan Lafere <nathanlafere@gmail.com>
2023-07-05 22:38:26 -0300

    save hotkeys

--- macro_sound_player.py
+++ macro_sound_player.py
@@ -15,7 +15,7 @@ def play_sound(file):
     while mixer.music.get_busy():  # wait for music to finish playing
         time.sleep(1)
 
-def create_hotkey():    
+def config_new_hotkey():    
     print("Choose the mp3 file!")
     file = askopenfilename()
     if file == '':
@@ -24,12 +24,21 @@ def create_hotkey():
     print(f"-=-= {file} =-=-")
     while True:
         hotkey = input("Enter the key to bind (Ex. shift+1): ")
-        if input(f'Are you sure you want to use {hotkey} as a hotkey? (y/n) ')[0].lower() == 'y':
+        if input(f'Are you sure you want to use {hotkey} as a hotkey? (y/n) ')[:1].lower() == 'y':
             break
-    hotkey_list.append([hotkey,file[file.rfind('/')+1:]])
+    os.system('copy '+file.replace("/","\\")+f' {os.getcwd()}\SoundFiles\\'+file[file.rfind('/')+1:file.rfind('.mp3')]+f'_{hotkey.replace("+","-")}.mp3')
+    hotkey_list.append([hotkey,file[file.rfind('/')+1:file.rfind('.mp3')],file])
     prompt_text()
+    create_hotkey(hotkey, file)
+
+def create_hotkey(hotkey,file):
     keyboard.add_hotkey(hotkey, lambda: play_sound(file))
-    
+
+def load_hotkey():
+    for file in os.listdir(os.getcwd()+'\SoundFiles'):
+        hotkey_list.append([file[file.rfind('_')+1:file.rfind('.mp3')].replace("-","+"),file[:file.rfind('_')],os.getcwd()+'\SoundFiles\\'+file])
+        create_hotkey(hotkey_list[-1][0],hotkey_list[-1][2])
+
 def prompt_text():
     print('ctrl+alt+1 = new hotkey', end='')
     for _hotkey in hotkey_list:
@@ -37,6 +46,7 @@ def prompt_text():
     print()
 
 hotkey_list = []
+load_hotkey()
 prompt_text()
-keyboard.add_hotkey("ctrl+alt+1", create_hotkey)
+keyboard.add_hotkey("ctrl+alt+1", config_new_hotkey)
 keyboard.wait('esc')
